f62e5ff4c575883355d68dc88a5ac2a3ca6a6bd0
Chris Carr <coder.knot@gmail.com>
2017-06-21 16:09:25 -0700

    add Cheapshark constants

--- app/src/main/java/com/coderknot/gamesearch/Constants.java
+++ app/src/main/java/com/coderknot/gamesearch/Constants.java
@@ -1,4 +1,13 @@
 package com.coderknot.gamesearch;
 
 public class Constants {
+
+    // Cheapshark API
+    public static final String CHEAPSHARK_BASE_URL = "http://www.cheapshark.com/api/1.0/";
+    public static final String CHEAPSHARK_RESOURCE_GAMES =  "games";
+    public static final String CHEAPSHARK_RESOURCE_DEALS = "deals";
+    public static final String CHEAPSHARK_TITLE_PARAMETER = "title";
+    public static final String CHEAPSHARK_SORTBY_PARAMETER = "sortBy";
+    public static final String CHEAPSHARK_DESC_PARAMETER = "desc";
+
 }
