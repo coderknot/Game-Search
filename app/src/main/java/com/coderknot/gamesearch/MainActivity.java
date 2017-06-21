9e45e9db7acd21f0c513495016f093722e87f373
Chris Carr <coder.knot@gmail.com>
2017-06-21 15:30:51 -0700

    add click event to Game Search button

--- app/src/main/java/com/coderknot/gamesearch/MainActivity.java
+++ app/src/main/java/com/coderknot/gamesearch/MainActivity.java
@@ -1,13 +1,32 @@
 package com.coderknot.gamesearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.gameSearchButton) Button mGameSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mGameSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mGameSearchButton) {
+            Intent gameSearchIntent = new Intent(MainActivity.this, GameSearchActivity.class);
+            startActivity(gameSearchIntent);
+        }
     }
 }
