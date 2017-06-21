cb67f4fb70daf7421f0da6764992762f76d4cf97
Chris Carr <coder.knot@gmail.com>
2017-06-21 16:40:51 -0700

    add initial Cheapshark API call

--- app/src/main/java/com/coderknot/gamesearch/GamesListActivity.java
+++ app/src/main/java/com/coderknot/gamesearch/GamesListActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.coderknot.gamesearch.services.CheapsharkService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class GamesListActivity extends AppCompatActivity {
     private static final String TAG = GamesListActivity.class.getSimpleName();
 
@@ -17,5 +25,23 @@ public class GamesListActivity extends AppCompatActivity {
         String game = gameSearchIntent.getStringExtra("game");
 
         Log.v(TAG, game);
+        getGamesList(game);
+    }
+
+    private void getGamesList(String game) {
+        final CheapsharkService cheapsharkService = new CheapsharkService();
+
+        cheapsharkService.findGames(game, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.v(TAG, response.toString());
+                cheapsharkService.processGames(response);
+            }
+        });
     }
 }
--- app/src/main/java/com/coderknot/gamesearch/services/CheapsharkService.java
+++ app/src/main/java/com/coderknot/gamesearch/services/CheapsharkService.java
@@ -1,4 +1,58 @@
 package com.coderknot.gamesearch.services;
 
+import android.util.Log;
+
+import com.coderknot.gamesearch.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class CheapsharkService {
+    public static final String TAG = CheapsharkService.class.getSimpleName();
+
+    public static final String SORTBY_VALUE = "Title";
+    public static final String DESC_VALUE = "0";
+
+    public static void findGames(String title, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.CHEAPSHARK_BASE_URL + Constants.CHEAPSHARK_RESOURCE_DEALS).newBuilder();
+        urlBuilder.addQueryParameter(Constants.CHEAPSHARK_TITLE_PARAMETER, title);
+        urlBuilder.addQueryParameter(Constants.CHEAPSHARK_SORTBY_PARAMETER, SORTBY_VALUE);
+        urlBuilder.addQueryParameter(Constants.CHEAPSHARK_DESC_PARAMETER, DESC_VALUE);
+        String url = urlBuilder.build().toString();
+
+        Log.v(TAG, url);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public void processGames(Response response) {
+        Log.v(TAG, "in processGames");
+        try {
+            if(response.isSuccessful()) {
+                String jsonData = response.body().string();
+                JSONArray gamesJSON = new JSONArray(jsonData);
+                Log.v(TAG, String.valueOf(gamesJSON.length()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
