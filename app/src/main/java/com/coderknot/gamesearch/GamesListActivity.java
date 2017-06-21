2736a4d3e808b5e2cb5ceae63b1b203f2dffcff0
Chris Carr <coder.knot@gmail.com>
2017-06-21 15:50:14 -0700

    capture searched game through passed intent

--- app/src/main/java/com/coderknot/gamesearch/GamesListActivity.java
+++ app/src/main/java/com/coderknot/gamesearch/GamesListActivity.java
@@ -1,13 +1,21 @@
 package com.coderknot.gamesearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GamesListActivity extends AppCompatActivity {
+    private static final String TAG = GamesListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_list);
+
+        Intent gameSearchIntent = getIntent();
+        String game = gameSearchIntent.getStringExtra("game");
+
+        Log.v(TAG, game);
     }
 }
